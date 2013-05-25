a5d0d541b924ddb97889945554b2b2b3b93a1ace
Jack Viers <jackcviers@gmail.com>
2013-05-25 15:53:42 -0500

    Adds project initialization template.

---
+++ harder_better_faster_stronger.js
@@ -0,0 +1,10 @@
+// harder_better_faster_stronger 0.0.1
+//
+// (c) 2013 Jack Viers
+// Licensed under the MIT license.
+
+(function(Backbone) {
+
+  // Your code goes here...
+
+}).call(this, Backbone);
---
+++ test/harder_better_faster_stronger.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+
+  module('Backbone.Harder_better_faster_stronger');
+
+});
