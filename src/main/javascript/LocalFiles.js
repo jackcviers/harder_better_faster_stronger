6dffa31885141f328a364c8de60373c96f9c9967
Jack Viers <jackcviers@gmail.com>
2013-06-11 22:13:07 -0500

    Adds LocalFiles collection stub.

---
+++ src/main/javascript/LocalFiles.js
@@ -0,0 +1,12 @@
+var jQuery = global.jQuery;
+jQuery = jQuery || require('jquery-browserify');
+var $ = jQuery;
+var _ = require('underscore');
+var Backbone = require('backbone');
+Backbone.$ = $;
+var File = require('./Backbone.File.Sync').File;
+var MusicFile = require('./MusicFile');
+
+var LocalFiles = {};
+
+module.exports = LocalFiles;
