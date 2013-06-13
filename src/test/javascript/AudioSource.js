cfeaaccaf480ae91126a45cce4a347a75810f8d2
Jack Viers <jackcviers@gmail.com>
2013-06-12 23:20:35 -0500

    Adds AudioSource test suite preparation code.

--- src/test/javascript/AudioSource.js
+++ src/test/javascript/AudioSource.js
@@ -1,1 +1,20 @@
-//empty file
+var global = global ||  window;
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var expect = chai.expect;
+var should = chai.should();
+chai.use(sinonChai);
+var jQuery = global.jQuery;
+jQuery = jQuery || require('jquery-browserify');
+var $ = jQuery;
+var _ = global._;
+_ = _ || require('underscore');
+var Backbone = global.Backbone;
+Backbone = Backbone ||  require('backbone');
+Backbone.$ = $
+var File = require('../../main/javascript/Backbone.File.Sync.js').File;
+Backbone = _.extend(Backbone, File);
+var when = require('when');
+var testInBrowserOnly = require('./testInBrowserOnly.js');
+
