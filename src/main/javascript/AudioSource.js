872106421e354cd4180cad5c358236d91605c1c4
Jack Viers <jackcviers@gmail.com>
2013-06-13 00:41:38 -0500

    Implements AudioSource.source element.

--- src/main/javascript/AudioSource.js
+++ src/main/javascript/AudioSource.js
@@ -6,17 +6,23 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 Backbone.$ = $;
 var File = require('./Backbone.File.Sync').File;
-var AudioContext = global.AudioContext || global.webkitAudioContext;
+// There can be only one per window!!!
+var AudioContext = (global.AudioContext || global.webkitAudioContext);
+var audioContext = new AudioContext();
 
 var AudioSource = Backbone.View.extend({
   audioContext: null,
+  analyzer: null,
   render: function(){
     if(this.audioContext === null){
-      this.audioContext = new AudioContext();
+      // create the audio context
+      this.audioContext = audioContext;
     }
+    this.source = this.audioContext.createBufferSource();    
     this.$el.html('<audio id="audio-source"></audio>');
     return this;
-  }
+  },
+  source: null
 });
 
 module.exports = AudioSource;
