fd72a804ebdedd9b21b1e2020408a9e09b903bc1
Jack Viers <jackcviers@gmail.com>
2013-06-12 23:27:33 -0500

    Adds failing type check test for AudioSource view.

--- src/test/javascript/AudioSource.js
+++ src/test/javascript/AudioSource.js
@@ -20,9 +20,20 @@ var testInBrowserOnly = require('./testInBrowserOnly.js');
 var AudioSource = require('../../main/javascript/AudioSource.js');
 
 describe('AudioSource', function(){
+  beforeEach(function(done){
+    this.instance = {};
+    done();
+  });
+  afterEach(function(done){
+    this.instance = {};
+    done();
+  });
   it('should exist', function(done){
     expect(AudioSource).to.exist;
     done();
   });
-  it('should be a Backbone.View');
+  it('should be a Backbone.View', function(done){
+    this.instance.should.be.an.instanceof(Backbone.View);
+    done();
+  });
 });
